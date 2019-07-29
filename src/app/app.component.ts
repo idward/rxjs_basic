de6d95a8004fbaf4c267fad3cc15421cc09d2fb5
罗晟 <wb-ls553774@alibaba-inc.com>
2019-07-29 17:46:12 +0800

    update files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { mapTo } from 'rxjs/operators';
+import { timer, concat } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'rxjs-basic';
+
+  ngOnInit(): void {
+    const getPostOne$ = timer(3000).pipe(mapTo({ id: 1 }));
+    const getPostTwo$ = timer(1000).pipe(mapTo({ id: 2 }));
+
+    concat(getPostOne$,getPostTwo$).subscribe(res => console.log(res));
+  }
 }
